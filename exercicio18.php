f29dcc0850108e9fb3b4e168b66ea1b40a9fa842
LUIS FELIPE MARTINS ALARCON <luis.alarcon@buscapecompany.com.br>
2018-11-11 16:54:05 -0200

    ordenando arrays

--- exercicio18.php
+++ exercicio18.php
@@ -1,46 +1,46 @@
 <?php
-$times = array('Palmeiras',
-                'Internacional',
-                'Flamengo',
-                'São',
-                'Grêmio',
-                'Atlético',
-                'Atlético',
-                'Santos',
-                'Cruzeiro',
-                'Botafogo',
-                'Fluminense',
-                'Corinthians',
-                'Bahia',
-                'Vasco',
-                'Ceará',
-                'Sport',
-                'Vitória',
-                'América',
-                'Chapecoense',
-                'Paraná');
-asort($times);
+$personalidades =  array('Maomé',
+                'Isaac Newton',
+                'Jesus de Nazaré',
+                'Sidarta Gautama',
+                'Confúcio',
+                'Paulo de Tarso',
+                'Cai Lun',
+                'Johannes Gutenberg',
+                'Cristovão Colombo',
+                'Albert Einstein',
+                'Louis Pasteur',
+                'Galileu Galilei',
+                'Aristóteles',
+                'Euclides',
+                'Moisés',
+                'Charles Darwin',
+                'Qin Shihuang',
+                'Augusto',
+                'Nicolau Copérnico',
+                'Antoine Lavoisier');
+asort($personalidades);
 
 echo "<table> 
         <tr><th> Ordem crescente</th></tr>";
-foreach($times as $time){
-    echo "<tr><td>{$time}</td></tr>";
+foreach($personalidades as $personalidade){
+    echo "<tr><td>{$personalidade}</td></tr>";
 }
 echo "</table>";
 
-arsort($times);
+arsort($personalidades);
 
 echo "<table> 
         <tr><th> Ordem decrescente</th></tr>";
-foreach($times as $time){
-    echo "<tr><td>{$time}</td></tr>";
-}
+foreach($personalidades as $personalidade){
+            echo "<tr><td>{$personalidade}</td></tr>";
+    }
 echo "</table>";
 
-shuffle($times);
+shuffle($personalidades);
 echo "<table> 
         <tr><th> Ordem sortida</th></tr>";
-foreach($times as $time){
-    echo "<tr><td>{$time}</td></tr>";
-}
+foreach($personalidades as $personalidade){
+            echo "<tr><td>{$personalidade}</td></tr>";
+        }
 echo "</table>";
