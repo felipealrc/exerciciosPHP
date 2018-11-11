fb455a616f95b761d0d85e71b35d316c97fea649
LUIS FELIPE MARTINS ALARCON <luis.alarcon@buscapecompany.com.br>
2018-11-11 18:12:32 -0200

    arrays associativos

---
+++ exercicio22.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$sp = array(
+    'aniversario'=> '25/01',
+    'fundacao' => '1554',
+    'gentilico' => 'paulista',
+    'lema'=>'Não sou conduzido, conduzo',
+    'padroeiro' => 'São Paulo de Tarso e Nossa Senhora da Penha de França',
+    'prefeito' => 'Bruno Covas'
+);
+
+
+
