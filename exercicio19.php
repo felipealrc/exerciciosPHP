d085b905b5e53a726b0b17039a4d9c7f51fe1b99
LUIS FELIPE MARTINS ALARCON <luis.alarcon@buscapecompany.com.br>
2018-11-11 17:12:39 -0200

    trabalhando com current e next

---
+++ exercicio19.php
@@ -0,0 +1,24 @@
+<?php 
+
+$capitais = array('Rio Branco',
+'Macapá',
+'Manaus',
+'Belém',
+'Porto Velho',
+'Boa Vista',
+'Palmas',
+'Maceió',
+'Salvador',
+'Fortaleza');
+
+echo current($capitais).PHP_EOL;
+echo next($capitais).PHP_EOL;
+echo next($capitais).PHP_EOL;
+echo next($capitais).PHP_EOL;
+echo next($capitais).PHP_EOL;
+echo next($capitais).PHP_EOL;
+echo next($capitais).PHP_EOL;
+echo next($capitais).PHP_EOL;
+echo next($capitais).PHP_EOL;
+echo next($capitais).PHP_EOL;
+echo next($capitais).PHP_EOL;
