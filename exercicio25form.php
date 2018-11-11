26793fbfd0271fb5d3433be7bb9387f40e2b14f3
LUIS FELIPE MARTINS ALARCON <luis.alarcon@buscapecompany.com.br>
2018-11-11 20:35:17 -0200

    alteracao para select

--- exercicio25form.php
+++ exercicio25form.php
@@ -1,7 +1,11 @@
 <form action="exercicio25process.php" method="POST">
     <input type="text" name="nome" placeholder="Nome"/>
     <input type="text" name="sobrenome" placeholder="Sobrenome"/>
-    <input type="text" name="turma" placeholder="Turma"/>
+    <select name="turma">
+        <option value="ADM">Administração - ADM</option>
+        <option value="ADS">Análise de Sistemas - ADS</option>
+        <option value="LOG">Logistica - LOG</option>
+    </select>
     <select name="periodo">
         <option value="manha">Manhã</option>
         <option value="tarde">Tarde</option>
